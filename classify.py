157c2a4038444b98085f666eaa536c31a46f9bbf
Aly Khimani <aly.khimani8@gmail.com>
2026-08-01 00:25:19 +0100

    Standardised matrix calculations

--- classify.py
+++ classify.py
@@ -1,3 +1,4 @@
+from functions import *
 import numpy as np
 from numba import njit
 from configparser import ConfigParser # to read config file
@@ -6,157 +7,52 @@ import os
 import csv
 
 @njit
-def int_to_vec(n, i): # converts an integer to its vector form
-    v = np.zeros(n, dtype=np.uint8)
-    for j in range(n):
-        v[j] = (i >> j) & 1 # right shift to get jth bit
-    return v
-
-@njit
-def vec_to_int(n, v): # converts a vector to its respective integer
-    i = 0
-    for j in range(n):
-        i += v[j]*(2**j)
-    return i
-
-@njit
-def getRank(M): # returns the rank of a matrix M
-    A = M.copy() # does not mainpulate original matrix
-    n, m = M.shape # the no. of rows and columns
-    rank = 0
-
-    for c in range(m): # for each column c
-        pivot = -1
-        for r in range(rank, n): # finds a row r that has a 1 in column c
-            if A[r, c] == 1:
-                pivot = r # sets the pivot to the row
-                break
-        
-        if pivot != -1: # if column c is not all 0s
-            if pivot != rank: # swaps pivot row into rank row
-                temp = A[rank].copy()
-                A[rank] = A[pivot]
-                A[pivot] = temp
-
-            for r in range(n): # eliminates column c from all non-pivot rows
-                if r != rank and A[r, c] == 1:
-                    A[r] = A[r]^A[rank] # XORs row with the pivot row
-            
-            rank += 1
-            if rank == n: # rank can't be more than the no. of rows
-                return rank
-
-    return rank
-
-@njit
-def getInverse(M): # returns the inverse of a square matrix M
-    A = M.copy() # does not mainpulate original matrix
-    n = M.shape[0] # the no. of rows and columns
-    I = np.eye(n, dtype=np.uint8)
-    rank = 0
-
-    for c in range(n): # for each column c
-        pivot = -1
-        for r in range(rank, n): # finds a row r that has a 1 in column c
-            if A[r, c] == 1:
-                pivot = r # sets the pivot to the row
-                break
-        
-        if pivot != -1: # if column c is not all 0s
-            if pivot != rank: # swaps pivot row into rank row
-                temp = A[rank].copy()
-                A[rank] = A[pivot]
-                A[pivot] = temp
-
-                temp = I[rank].copy()
-                I[rank] = I[pivot]
-                I[pivot] = temp
-
-            for r in range(n): # eliminates column c from all non-pivot rows
-                if r != rank and A[r, c] == 1:
-                    A[r] = A[r]^A[rank] # XORs row with the pivot row
-                    I[r] = I[r]^I[rank]
-
-            rank += 1
-
-    if rank < n:
-        return np.zeros(shape=(n, n), dtype=np.uint8)
-    
-    return I
-
-@njit
-def getCanconicalForm(n, m, coeffs):
-    basis = np.zeros(shape = (n, n), dtype=np.uint8)
+def calculate_invertibles(n): # gets all invertible matrices in GF(2^n)
+    invertibles = np.zeros(shape=(31*30*28*24*16, n), dtype=np.uint8) # hardcoded matrix allocation size for n=5
     count = 0
-    for i in range(m):
-        basis[count] = int_to_vec(n, coeffs[i])
-        if getRank(basis[:count+1]) == count+1:
-            count += 1
-            if count == n:
-                break
-
-    L1 = getInverse(basis.transpose())
-
-    G = np.zeros(shape = (m, n), dtype=np.uint8)
-    for i in range(m):
-        G[i] = int_to_vec(n, coeffs[i])
-
-    G2 = np.zeros(shape = (m, n), dtype=np.uint8)
-    for j in range(m):
-        for i in range(n):
-            for k in range(n):
-                G2[j, i] ^= L1[i, k] & G[j, k] # row * row (transposed column)
-
-    gPrime = np.zeros(m, dtype=np.uint8)
-    for i in range(m):
-        gPrime[i] = vec_to_int(n, G2[i])
-
-    return gPrime
-
-@njit
-def calculateInvertibles(n): # gets all invertible matrices in GF(2^n)
-    invertibles = np.zeros(shape=(31*30*28*24*16, n, n), dtype=np.uint8)
-    vectors = np.zeros(shape = (2**n-1, n), dtype=np.uint8) # generates all vectors in GF(2^n)
-    for i in range(1, 2**n): # only do this computation once per n
-        vectors[i-1] = int_to_vec(n, i)
+    N = 2**n
+    M = np.zeros(n, dtype=np.int64)
     
-    count = 0
-    for c1 in vectors:
-        M = np.zeros(shape=(n, n), dtype=np.uint8)
+    for c1 in range(1, N):
         M[0] = c1
-        for c2 in vectors:
+        for c2 in range(1, N):
             M[1] = c2
-            if getRank(M[:2]) == 2:
-                for c3 in vectors:
+            if get_rank(n, 2, M[:2]) == 2:
+                for c3 in range(1, N):
                     M[2] = c3
-                    if getRank(M[:3]) == 3:
-                        for c4 in vectors:
+                    if get_rank(n, 3, M[:3]) == 3:
+                        for c4 in range(1, N):
                             M[3] = c4
-                            if getRank(M[:4]) == 4:
-                                for c5 in vectors:
+                            if get_rank(n, 4, M[:4]) == 4:
+                                for c5 in range(1, N):
                                     M[4] = c5
-                                    if getRank(M) == 5:
-                                        invertibles[count] = M
+                                    if get_rank(n, 5, M) == 5:
+                                        invertibles[count, 0] = c1
+                                        invertibles[count, 1] = c2
+                                        invertibles[count, 2] = c3
+                                        invertibles[count, 3] = c4
+                                        invertibles[count, 4] = c5
                                         count += 1
 
     return count, invertibles
 
 @njit
-def getCubicCoeffs(n, m): # gets all cubic indicies
-    cubicCoeffs = np.zeros(m, dtype=np.uint8)
+def get_cubic_coeffs(n, m): # gets all cubic indicies
+    cubic_coeffs = np.zeros(m, dtype=np.uint64)
     count = 0
     for i in range(n):
         for j in range(i+1, n):
             for k in range(j+1, n):
-                cubicCoeffs[count] = 2**i + 2**j + 2**k
+                cubic_coeffs[count] = 2**i + 2**j + 2**k
                 count += 1
-    return cubicCoeffs
 
-def getFunctions(n, m): # gets all optimal functions in GF(2^n) into an array
+    return cubic_coeffs
+
+def get_functions(n, m): # gets all optimal functions in GF(2^n) into an array
     with open('search/optimal.csv', 'r') as f:
         lines = f.readlines()
 
-    funcs = np.zeros(shape=(len(lines), m), dtype=np.uint8)
+    funcs = np.zeros(shape=(len(lines), m), dtype=np.uint64)
     count = 0 # no. of optimal functions
     for line in lines[1:]:
         data = line.strip().split(',') # gets one function without trailing spaces
@@ -169,63 +65,66 @@ def getFunctions(n, m): # gets all optimal functions in GF(2^n) into an array
     return count, funcs[:count]
 
 @njit
-def packFunc(m, func): # converts a function to a 64-bit integer
+def pack_func(m, func): # converts a function to a 64-bit integer
     res = np.uint64(0)
     for i in range(m):
         res = (res << np.uint64(5) | np.uint64(func[i]))
     return res
 
 @njit
-def unpackFunc(m, packed): # converts an integer back to its coefficients
-    func = np.zeros(m, dtype=np.uint8)
-    for i in range(m - 1, -1, -1):
-        func[i] = np.uint8(packed & np.uint64(0x1F))
-        packed >>= np.uint64(5)
-    return func
-
-@njit
-def getTT(n, m, cubicCoeffs, coeffs): # returns the truth table of coeffs
-    tt1 = np.zeros(shape = (2**n, n), dtype=np.uint8)
-    for x in range(2**n):
+def get_tt(n, m, cubic_coeffs, coeffs): # returns the 1D truth table of coeffs where each entry is a bit-packed integer
+    N = 2**n
+    tt1 = np.zeros(N, dtype=np.uint64)
+    for x in range(N):
         for c in range(m): # cubic coefficients only
-            if x & cubicCoeffs[c] == cubicCoeffs[c]:
+            if (x & cubic_coeffs[c]) == cubic_coeffs[c]:
                 tt1[x] ^= coeffs[c]
+
     return tt1
 
 @njit
-def getG(n, m, cubicCoeffs, inv, vectors, tt1, funcsPacked, activeMask, high): # multiplies a function from its truth table by an invertible matrix
-    tt2 = np.zeros(shape = (2**n, n), dtype=np.uint8) # truth table of L_2 (i.e. inv)
-    for i in range(2**n): # for each input i
-        for j in range(n): # for each col in inv
-            for k in range(n): # for each row in inv
-                tt2[i, j] ^= vectors[i, k] & inv[k, j] # multiply instead of XOR
+def get_g(n, m, cubic_coeffs, inv, tt1, funcs_packed, active_mask, high): # multiplies a function from its truth table by an invertible matrix
+    N = 2**n
+    tt2 = np.zeros(N, dtype=np.uint64) # truth table of L_2 (i.e. inv)
 
-    anf = np.zeros(shape = (2**n, n), dtype=np.uint8) # truth table of F*L_2
-    for i in range(2**n):
-        anf[i] = tt1[vec_to_int(n, tt2[i])]
+    # multiplies the input vector 'x' by the matrix 'inv'
+    for x in range(N): # for each input i
+        val = 0
+        for row_idx in range(n):
+            if (x >> row_idx) & 1: # if the row_idx-th bit of x is 1
+                val ^= inv[row_idx]
+        tt2[x] = val
 
-    for i in range(0, n): # butterfly algorithm
+    # re-maps the truth table
+    anf = np.zeros(N, dtype=np.uint64) # truth table of F*L_2
+    for i in range(N):
+        anf[i] = tt1[tt2[i]]
+
+    # butterfly algorithm to convert the truth table into ANF
+    for i in range(n):
         step = 2**i
-        for j in range(0, 2**n, step*2):
-            anf[j+step : j+2*step, :] ^= anf[j : j+step, :]
+        for j in range(0, N, step*2):
+            for k in range(j, j+step):
+                anf[k+step] ^= anf[k]
 
-    coeffs = np.zeros(m, dtype=np.uint8)
+    # extracts coeffs and maps to canonical form
+    coeffs = np.zeros(m, dtype=np.uint64)
     for i in range(m):
-        coeffs[i] = vec_to_int(n, anf[cubicCoeffs[i]])
+        coeffs[i] = anf[cubic_coeffs[i]]
 
-    gPrime = getCanconicalForm(n, m, coeffs)
-    gPrimePacked = packFunc(m, gPrime)
+    g_prime = get_canonical_form(n, m, coeffs)
+    g_prime_packed = pack_func(m, g_prime)
 
     low = 0
     while low <= high:
         mid = (low + high)//2
-        if funcsPacked[mid] == gPrimePacked:
-            if activeMask[mid]:
-                activeMask[mid] = False
+        if funcs_packed[mid] == g_prime_packed:
+            if active_mask[mid]:
+                active_mask[mid] = False
                 return 1
             else:
                 return 0
-        elif funcsPacked[mid] < gPrimePacked:
+        elif funcs_packed[mid] < g_prime_packed:
             low = mid + 1
         else:
             high = mid - 1
@@ -235,51 +134,43 @@ def getG(n, m, cubicCoeffs, inv, vectors, tt1, funcsPacked, activeMask, high): #
 def main(n):
     m = int((n*(n-1)*(n-2))/6) # number of coeffs, i.e. nC3
     
-    countInv, invertibles = calculateInvertibles(n)
-    cubicCoeffs = getCubicCoeffs(n, m)
+    count_inv, invertibles = calculate_invertibles(n)
+    cubic_coeffs = get_cubic_coeffs(n, m)
 
-    vectors = np.zeros(shape = (2**n, n), dtype=np.uint8) # generates all vectors in GF(2^n)
-    for i in range(2**n): # only do this computation once per n
-        vectors[i] = int_to_vec(n, i)
+    total_func, funcs = get_functions(n, m)
+    funcs_packed = np.array([pack_func(m, f) for f in funcs], dtype=np.uint64) # converts all functions into integers
 
-    totalFunc, funcs = getFunctions(n, m)
-    funcsPacked = np.array([packFunc(m, f) for f in funcs], dtype=np.uint64) # converts all functions into integers
-    sort_idx = np.argsort(funcsPacked)
-    funcsPacked = funcsPacked[sort_idx]
+    sort_idx = np.argsort(funcs_packed)
+    funcs_packed = funcs_packed[sort_idx]
     funcs = funcs[sort_idx]
-    activeMask = np.ones(totalFunc, dtype=np.bool_) # create a boolean mask to track which functions are still active
+    active_mask = np.ones(total_func, dtype=np.bool_) # create a boolean mask to track which functions are still active
 
-    countFunc = 0
-    while countFunc < totalFunc:
-        for idx in range(totalFunc):
-            if activeMask[idx]:
+    count_func = 0
+    while count_func < total_func:
+        for idx in range(total_func):
+            if active_mask[idx]:
                 func = funcs[idx]
                 break
 
-        coeffs = np.zeros(shape = (m, n), dtype=np.uint8) # coeffs = [[coeffs of x1x2x3], ..., [coeffs of x(n-2)x(n-1)xn]]
-        for j in range(m): # for each cubic coefficient
-            coeffs[j] = int_to_vec(n, func[j]) # adds coefficient in vector form
-        tt1 = getTT(n, m, cubicCoeffs, coeffs) # gets truth table of coeffs
+        tt1 = get_tt(n, m, cubic_coeffs, func) # gets truth table of coeffs
 
         print('\nChecking function', func)
-        with tqdm.tqdm(total=totalFunc, initial=countFunc, desc='Remaining Functions') as pbarFunc:
-            with tqdm.tqdm(total = countInv, desc='Applying invertibles') as pbarInv:
-                for k in range(countInv):
-                    num = getG(n, m, cubicCoeffs, invertibles[k], vectors, tt1, funcsPacked, activeMask, totalFunc-1)
+        with tqdm.tqdm(total=total_func, initial=count_func, desc='Remaining Functions') as pbarFunc:
+            with tqdm.tqdm(total=count_inv, desc='Applying invertibles') as pbarInv:
+                for k in range(count_inv):
+                    num = get_g(n, m, cubic_coeffs, invertibles[k], tt1, funcs_packed, active_mask, total_func-1)
                     if num == 1:
-                        countFunc += 1
+                        count_func += 1
                         pbarFunc.update()
                     pbarInv.update()
 
         if not os.path.exists('optimal' + str(n) + '.csv'): # writes headers if file does not exist
             with open('optimal' + str(n) + '.csv', 'a', newline='') as f:
-                writer = csv.writer(f)
-                writer.writerow(['n', 'coeffs'])
+                csv.writer(f).writerow(['n', 'coeffs'])
             
         with open('optimal' + str(n) + '.csv', 'a', newline='') as f:
-            writer = csv.writer(f)
             func_string = '|'.join(map(str, func))
-            writer.writerow([n, func_string])
+            csv.writer(f).writerow([n, func_string])
     
 if __name__ == "__main__":
     # reads options from config file
@@ -287,8 +178,8 @@ if __name__ == "__main__":
     config.read('config.ini')
     n = config.get('classify', 'n')
 
-    #try:
-    n = int(n) # ensures that n is an integer
-    main(n)
-    #except:
-    #    print('Error - min and max must be integers')
+    try:
+        n = int(n) # ensures that n is an integer
+        main(n)
+    except ValueError:
+        print('Error - n must be an integer')
